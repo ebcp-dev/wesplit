cf1fe129c02460b199da31a41008240d91a87cb2
Earl Perez <ebcp.dev@gmail.com>
2020-03-25 01:35:48 -0700

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Earl Perez on 3/25/20.
+//  Copyright © 2020 Earl Perez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
